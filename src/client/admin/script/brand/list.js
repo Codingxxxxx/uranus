0628b74a385fa9ad4f8d2b8195989930c82a3fe5
Minea <hc2.minea@gmail.com>
2024-06-02 20:53:28 +0700

    fix(admin): fixed typo on function formaTableDate to formatTableDate

--- src/client/admin/script/brand/list.js
+++ src/client/admin/script/brand/list.js
@@ -2,7 +2,7 @@ import { jQuery } from '../../../shared/libs/jquery';
 import { getDefaultContent } from '../core/constant';
 import { createColumnImage } from '../core/custom-datatable';
 import { getBrandImageObjectPath } from '../core/cdn';
-import { formaTableDate } from '../core/date-utils';
+import { formatTableDate } from '../core/date-utils';
 
 const tableId = '#brandTable';
 
@@ -35,7 +35,7 @@ function initDataTable() {
         data: 'createdAt',
         render: (data) => {
           if (!data) return;
-          return formaTableDate(data) || getDefaultContent();
+          return formatTableDate(data) || getDefaultContent();
         }
       },
       {
--- src/client/admin/script/user/list.js
+++ src/client/admin/script/user/list.js
@@ -2,7 +2,7 @@ import { jQuery } from '../../../shared/libs/jquery';
 import { getDefaultContent } from '../core/constant';
 import { createColumnImage } from '../core/custom-datatable';
 import { getBrandImageObjectPath } from '../core/cdn';
-import { formaTableDate } from '../core/date-utils';
+import { formatTableDate } from '../core/date-utils';
 
 const tableId = '#tableUser';
 
@@ -31,7 +31,7 @@ function initDataTable() {
         data: 'createdAt',
         render: (data) => {
           if (!data) return;
-          return formaTableDate(data);
+          return formatTableDate(data);
         }
       },
       {
@@ -47,7 +47,7 @@ function initDataTable() {
         data: 'lastLogin',
         render: (data) => {
           if (!data) return;
-          return formaTableDate(data);
+          return formatTableDate(data);
         }
       }
     ]
